783603a1101df5dc5bfe1b2fac16a5c004352665
Siva-K67 <avishawkings@gmail.com>
2026-08-06 14:44:21 +0530

    Added mutexes for concurrency safety

--- http-server/main.go
+++ http-server/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync" // ADDED: needed for Mutex
 )
 
 type InMemoryPlayerStore struct {
 	store map[string]int
+	lock  sync.Mutex // ADDED: guards `store` against concurrent access
 }
 
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
@@ -14,14 +16,18 @@ func NewInMemoryPlayerStore() *InMemoryPlayerStore {
 }
 
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.lock.Lock()         // ADDED
+	defer i.lock.Unlock() // ADDED
 	return i.store[name]
 }
 
 func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.lock.Lock()         // ADDED
+	defer i.lock.Unlock() // ADDED
 	i.store[name]++
 }
 
 func main() {
-	server := &PlayerServer{NewInMemoryPlayerStore()}
-	log.Fatal(http.ListenAndServe(":5000", server))
+	server := &PlayerServer{NewInMemoryPlayerStore()} // UNCHANGED
+	log.Fatal(http.ListenAndServe(":5000", server))   // UNCHANGED
 }
